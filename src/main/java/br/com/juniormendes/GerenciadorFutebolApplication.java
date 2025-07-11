3b4ed2c3c9fcfab98114e9a45a54b38fcb4855d6
Junior Mendes <juniormendesjp@gmail.com>
2025-07-10 22:29:39 -0300

    criação do projeto

---
+++ src/main/java/br/com/juniormendes/GerenciadorFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.juniormendes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorFutebolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juniormendes/GerenciadorFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juniormendes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
